60306450a65738cf0668af56cdec29569c070209
Nick Friend <nickmfriend@gmail.com>
2012-11-01 20:47:40 -0700

    reinitalize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nickblog::Application.config.secret_token = '5557cfa6fdd2721fc48511a42a8046eddc2915380765247102dc157b3f89e2456c984428815a745cbafd62ab90e267195468280dddb6e39549a10b11ca67b445'
